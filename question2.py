22e10f8031ddbed4da7e0b6f7672d8fc3b9edcba
Muhammad Yahya Sani <141664948+YahyaSani@users.noreply.github.com>
2025-12-24 14:30:14 +0500

    Add files via upload

---
+++ question2.py
@@ -0,0 +1,13 @@
+products-{"Electronics": ("Laptop": 1200, "Phone"s ase),"Clothes": ("shirt": 50, "Shoes": 100),"Grocery": ("Rice": 20, "Milk": 10)
+
+max price-o
+
+for category items in products.values():
+
+for name, price in category items.Item()
+
+if price prices
+
+max priceprice
+
+print(p_name", price)
